a606106713b78337282a447e04a48bd95638863a
Jirayu Aunkhonthee <126430616+chotchoterd@users.noreply.github.com>
2024-08-19 09:08:13 +0700

    commit

--- obj/Debug/net8.0/DotnetStockAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetStockAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetStockAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca5db09a957f049adbaa38cb64942d3e808d3da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465ecece9757cda51425fc5e3098572200ec5a82")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetStockAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetStockAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
