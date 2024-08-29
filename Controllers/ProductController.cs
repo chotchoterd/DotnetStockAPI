e2a5d3293e7c293f2dd472f4eceb26f6a7da6260
Jirayu Aunkhonthee <126430616+chotchoterd@users.noreply.github.com>
2024-08-29 08:47:54 +0700

    commits

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -6,32 +6,52 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DotnetStockAPI.Controllers;
 
+// Multiple Roles
+// [Authorize(Roles = UserRolesModel.Admin + "," + UserRolesModel.Manager)]
 [Authorize]
 [ApiController]
 [Route("api/[controller]")]
 [EnableCors("MultipleOrigins")]
-public class ProductController : ControllerBase
+public class ProductController: ControllerBase
 {
+    // สร้าง Object ของ ApplicationDbContext
     private readonly ApplicationDbContext _context;
+
+    // IWebHostEnvironment คืออะไร
+    // ContentRootPath: เส้นทางไปยังโฟลเดอร์รากของเว็บแอปพลิเคชัน
+    // WebRootPath: เส้นทางไปยังโฟลเดอร์ wwwroot ของเว็บแอปพลิเคชัน
     private readonly IWebHostEnvironment _env;
 
+    // ฟังก์ชันสร้าง Constructor รับค่า ApplicationDbContext
     public ProductController(ApplicationDbContext context, IWebHostEnvironment env)
     {
         _context = context;
         _env = env;
     }
 
-    [HttpGet("[action]")]
+    // ฟังก์ชันสำหรับการดึงข้อมูลสินค้าทั้งหมด
+    // GET /api/Product
+    [HttpGet]
     public ActionResult<product> GetProducts(
-        [FromQuery] int page = 1,
-        [FromQuery] int limit = 100,
-        [FromQuery] string? searchQuery = null,
-        [FromQuery] int? selectedCategory = null)
+        [FromQuery] int page=1, 
+        [FromQuery] int limit=100, 
+        [FromQuery] string? searchQuery=null,
+        [FromQuery] int? selectedCategory = null
+    )
     {
+        // skip คือ การข้ามข้อมูล
         int skip = (page - 1) * limit;
+
+        // LINQ สำหรับการดึงข้อมูลจากตาราง Products ทั้งหมด
         // var products = _context.products.ToList();
+
+        // แบบอ่านที่มีเงื่อนไข
+        // select * from products where unitinstock >= 10
         // var products = _context.products.Where(p => p.unitinstock >= 10).ToList();
-        var query = _context.products.Join(
+
+        // แบบเชื่อมกับตารางอื่น products เชื่อมกับ categories
+        var query = _context.products
+        .Join(
             _context.categories,
             p => p.categoryid,
             c => c.categoryid,
@@ -43,39 +63,53 @@ public class ProductController : ControllerBase
                 p.unitinstock,
                 p.productpicture,
                 p.categoryid,
+                p.createddate,
                 p.modifieddate,
                 c.categoryname
-            });
+            }
+        );
 
-        if (!string.IsNullOrEmpty(searchQuery))
+        // ถ้ามีการค้นหา
+        if(!string.IsNullOrEmpty(searchQuery))
         {
             query = query.Where(p => EF.Functions.ILike(p.productname!, $"%{searchQuery}%"));
         }
 
-        if (selectedCategory.HasValue)
+        // ถ้ามีการค้นหาตาม Category
+        if(selectedCategory.HasValue)
         {
             query = query.Where(p => p.categoryid == selectedCategory.Value);
         }
 
+        // นับจำนวนข้อมูลทั้งหมด
         var totalRecords = query.Count();
+
         var products = query
         .OrderByDescending(p => p.productid)
         .Skip(skip)
         .Take(limit)
         .ToList();
 
-        return Ok(new
-        {
-            Total = totalRecords,
-            Products = products
-        });
+        // ส่งข้อมูลกลับไปให้ Client เป็น JSON
+        return Ok(
+            new {
+                Total = totalRecords,
+                Products = products
+            }
+        );
     }
 
-    [HttpGet("[action]/{id}")]
+    // ฟังก์ชันสำหรับการดึงข้อมูลสินค้าตาม id
+    // GET /api/Product/1
+    [HttpGet("{id}")]
     public ActionResult<product> GetProduct(int id)
     {
+        // LINQ สำหรับการดึงข้อมูลจากตาราง Products ตาม ID
         // var product = _context.products.Find(id);
-        var product = _context.products.Join(
+
+        // แบบเชื่อมกับตารางอื่น products เชื่อมกับ categories
+        var product = _context.products
+        .Join(
             _context.categories,
             p => p.categoryid,
             c => c.categoryid,
@@ -87,27 +121,41 @@ public class ProductController : ControllerBase
                 p.unitinstock,
                 p.productpicture,
                 p.categoryid,
+                p.createddate,
                 p.modifieddate,
                 c.categoryname
-            }).FirstOrDefault(p => p.categoryid == id);
+            }
+        )
+        .FirstOrDefault(p => p.productid == id);
 
+        // ถ้าไม่พบข้อมูล
         if (product == null)
         {
             return NotFound();
         }
 
+        // ส่งข้อมูลกลับไปให้ Client เป็น JSON
         return Ok(product);
     }
 
-    [HttpPost("[action]")]
+    // ฟังก์ชันสำหรับการเพิ่มข้อมูลสินค้า
+    // POST: /api/Product
+    [HttpPost]
     public async Task<ActionResult<product>> CreateProduct([FromForm] product product, IFormFile? image)
     {
+        // เพิ่มข้อมูลลงในตาราง Products
         _context.products.Add(product);
-        if (image != null)
+
+        // ถ้ามีการอัพโหลดไฟล์
+        if(image != null)
         {
+            // กำหนดชื่อไฟล์รูปภาพใหม่
             string fileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+
+            // กำหนดเส้นทางไปยังโฟลเดอร์ uploads
             string uploadFolder = Path.Combine(_env.WebRootPath, "uploads");
 
+            // ตรวจสอบว่าโฟลเดอร์ uploads มีหรือไม่
             if (!Directory.Exists(uploadFolder))
             {
                 Directory.CreateDirectory(uploadFolder);
@@ -118,39 +166,50 @@ public class ProductController : ControllerBase
                 await image.CopyToAsync(fileStream);
             }
 
+            // บันทึกชื่อไฟล์รูปภาพลงในฐานข้อมูล
             product.productpicture = fileName;
-        }
-        else
-        {
+
+        } else {
             product.productpicture = "noimg.jpg";
         }
 
         _context.SaveChanges();
 
+        // ส่งข้อมูลกลับไปให้ Client เป็น JSON
         return Ok(product);
     }
 
-    [HttpPut("[action]/{id}")]
+    // ฟังก์ชันสำหรับการแก้ไขข้อมูลสินค้า
+    // PUT: /api/Product/1
+    [HttpPut("{id}")]
     public async Task<ActionResult<product>> UpdateProduct(int id, [FromForm] product product, IFormFile? image)
     {
-        // var productData = _context.products.Find(id);
+        // ดึงข้อมูลสินค้าตาม id
         var existingProduct = _context.products.FirstOrDefault(p => p.productid == id);
+
+        // ถ้าไม่พบข้อมูล
         if (existingProduct == null)
         {
             return NotFound();
         }
 
+        // แก้ไขข้อมูล
         existingProduct.productname = product.productname;
         existingProduct.unitprice = product.unitprice;
         existingProduct.unitinstock = product.unitinstock;
         existingProduct.categoryid = product.categoryid;
         existingProduct.modifieddate = product.modifieddate;
 
-        if (image != null)
+        // ตรวจสอบว่ามีการอัพโหลดไฟล์รูปภาพหรือไม่
+        if(image != null)
         {
+            // กำหนดชื่อไฟล์รูปภาพใหม่
             string fileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+
+            // กำหนดเส้นทางไปยังโฟลเดอร์ uploads
             string uploadFolder = Path.Combine(_env.WebRootPath, "uploads");
 
+            // ตรวจสอบว่าโฟลเดอร์ uploads มีหรือไม่
             if (!Directory.Exists(uploadFolder))
             {
                 Directory.CreateDirectory(uploadFolder);
@@ -161,39 +220,54 @@ public class ProductController : ControllerBase
                 await image.CopyToAsync(fileStream);
             }
 
-            if (existingProduct.productpicture != "noimg.jpg")
-            {
+            // ลบไฟล์รูปภาพเดิม ถ้ามีการอัพโหลดรูปภาพใหม่ และรูปภาพเดิมไม่ใช่ noimg.jpg
+            if(existingProduct.productpicture != "noimg.jpg"){
                 System.IO.File.Delete(Path.Combine(uploadFolder, existingProduct.productpicture!));
             }
 
+            // บันทึกชื่อไฟล์รูปภาพลงในฐานข้อมูล
             existingProduct.productpicture = fileName;
+
         }
 
+        // บันทึกการแก้ไขข้อมูล
         _context.SaveChanges();
 
+        // ส่งข้อมูลกลับไปให้ Client เป็น JSON
         return Ok(existingProduct);
     }
 
-    [HttpDelete("[action]/{id}")]
+    // ฟังก์ชันสำหรับการลบข้อมูลสินค้า
+    // DELETE /api/Product/1
+    [HttpDelete("{id}")]
     public ActionResult<product> DeleteProduct(int id)
     {
+        // ค้นหาข้อมูลจากตาราง Products ตาม ID
         var product = _context.products.Find(id);
 
+        // ถ้าไม่พบข้อมูล
         if (product == null)
         {
             return NotFound();
         }
 
-        if (product.productpicture != "noimg.jpg")
-        {
+        // ตรวจสอบว่ามีไฟล์รูปภาพหรือไม่
+        if(product.productpicture != "noimg.jpg"){
+            // string uploadFolder = Path.Combine(_env.ContentRootPath, "uploads");
             string uploadFolder = Path.Combine(_env.WebRootPath, "uploads");
 
+            // ลบไฟล์รูปภาพ
             System.IO.File.Delete(Path.Combine(uploadFolder, product.productpicture!));
         }
 
-        _context.products.Remove(product);
-        _context.SaveChanges();
+        // ลบข้อมูล
+        _context.products.Remove(product); // delete from products where id = 1
 
+        // บันทึกการลบข้อมูล
+        _context.SaveChanges(); // commit
+
+        // ส่งข้อมูลกลับไปให้ Client เป็น JSON
         return Ok(product);
     }
+
 }
--- obj/Debug/net8.0/DotnetStockAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetStockAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetStockAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a606106713b78337282a447e04a48bd95638863a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0d627ac01d5763624806560555474964d48304")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetStockAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetStockAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
